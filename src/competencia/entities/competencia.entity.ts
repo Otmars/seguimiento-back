a9bfae2290ce8a35bb0d4eb06344e27b1ab1899a
Otmars <otmars1998@gmail.com>
2023-04-26 22:03:22 -0400

    asignado competencias a materias

--- src/competencia/entities/competencia.entity.ts
+++ src/competencia/entities/competencia.entity.ts
@@ -1,1 +1,46 @@
-export class Competencia {}
+import { Asignatura } from 'src/asignatura/entities/asignatura.entity';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+export enum TipoCompentecia {
+  MACRO = 'macro',
+  GENERICAS = 'generica',
+  ESPECIFICAS = 'especifica',
+}
+
+@Entity('competencia')
+export class Competencia {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column({
+    nullable: false,
+  })
+  descripcion: string;
+
+  @Column({
+    type: 'enum',
+    enum: TipoCompentecia,
+    nullable: false,
+  })
+  tipoCompetencia: TipoCompentecia;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @ManyToMany(() => Asignatura,{cascade: true})
+  @JoinTable()
+  asignatura: Asignatura [];
+}
